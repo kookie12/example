7bd88a4013a02df2911c5b31134700f73f009a5a
kookie12 <good54826.dgist.ac.kr>
2022-04-26 20:54:02 +0900

    first test

---
+++ test.py
@@ -0,0 +1,41 @@
+print("test")
+
+# make CNN only using numpy 
+class CNN:
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.weights = []
+        self.biases = []
+        self.layers = []
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+        self.layers.append(self.weights)
+        self.layers.append(self.biases)
+        self.layers.append(self.input_shape)
+        self.layers.append(self.output_shape)
+    
